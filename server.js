c83ddf1e5fdad2702ffe0e2ffd1a73d8d208f511
Matthew Subin <slowlifematt@Matthews-MacBook-Pro.local>
2022-10-11 21:43:24 -0700

    create route for drinks and exported from models

--- server.js
+++ server.js
@@ -1,10 +1,14 @@
 const express = require(`express`);
-
+const drinks = require("./models/drinks")
 const app = express();
 const port = 3000;
 
 app.get('/', (req,res) => {
     res.send(`Welcome to the Gitpub App!`)
+})
+
+app.get('/drinks/', (req,res) => {
+    res.send(drinks)
 })
 
 
