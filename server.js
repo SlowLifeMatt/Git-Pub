a5131ff7d5bc1fd0e73df9137aa3225e88969ddd
Matthew Subin <slowlifematt@Matthews-MacBook-Pro.local>
2022-10-12 17:43:19 -0700

    Added image and drinks to show page

--- server.js
+++ server.js
@@ -15,7 +15,10 @@ app.get('/drinks/', (req,res) => {
 })
 
 app.get('/drinks/:id', (req,res) => {
-    res.send(req.params.id)
+    // res.send(req.params.id)
+    res.render('show.ejs', {
+        drinks: drinks[req.params.id],
+    })
 })
 
 
